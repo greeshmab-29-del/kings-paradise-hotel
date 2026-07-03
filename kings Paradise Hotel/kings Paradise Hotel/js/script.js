d1ac86d769294a8d68e74839b5f1e4cef1b8a7cb
greeshmab-29-del <greeshma.b-29@scds.saiuniversity.edu.in>
2026-07-03 20:06:10 +0530

    Add files via upload

---
+++ kings Paradise Hotel/kings Paradise Hotel/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("bookingForm").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    alert("Booking request submitted successfully!");
+
+});
+document.getElementById("bookingForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Thank you for choosing Kings Paradise Hotel! Your booking request has been submitted successfully.");
+});
